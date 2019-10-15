9dc1fe8acdc9eefc10d47390502b175a259bb6bb
Andrei <and.tutin@gmail.com>
2019-10-15 17:25:56 +0300

    начал работу над скиллами на vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,19 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+new Vue ({
+    el: "#skills",
+    data: {
+        frontend: {
+            'HTML': 25,
+            'CSS': 13,
+            'Javascript': 75,
+            'JQuery & Vue.js': 25 
+        },
+        workflow: {
+            'GIT': 13,
+            'Terminal': 25,
+            'Gulp': 50,
+            'Webpack': 35
+        }
+    }
+})
