23413f1e5e1be61388f1236b14901f8fb397c136
Andrei <and.tutin@gmail.com>
2019-10-16 23:28:16 +0300

    kinda works

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,51 +1,68 @@
 import Vue from 'vue';
 
-new Vue ({
-    el: "#skills",
-    data: {
-        skills: [
-            {
-                "category": "frontend",
-                "tech": [
-                    {
-                        "title": 'HTML',
-                        "percent": 25
-                    },
-                    {
-                        "title": 'CSS',
-                        "percent": 13
-                    },
-                    {
-                        "title": 'Javascript',
-                        "percent": 75
-                    },
-                    {
-                        "title": 'JQuery & Vue.js',
-                        "percent": 25
-                    }
-                ]
-            },
-            {
-                "category": "workflow",
-                "tech": [
-                    {
-                        "title": 'GIT',
-                        "percent": 13
-                    },
-                    {
-                        "title": 'Terminal',
-                        "percent": 25
-                    },
-                    {
-                        "title": 'Gulp',
-                        "percent": 50
-                    },
-                    {
-                        "title": 'Webpack',
-                        "percent": 35
-                    }
-                ]
-            }
-        ]
+var categoryRow = {
+    template: "#category-row-template",
+    props: ['category']
+}
+
+var skillCircle = {
+    template: "#skill-circle-template",
+    props: ['skill']
+}
+
+new Vue({
+    el: "#skills-block",
+    template: "#skills-list-template",
+    data() {
+        return {
+            skills: [
+                {
+                    "category": "frontend",
+                    "tech": [
+                        {
+                            "title": 'HTML',
+                            "percent": 90
+                        },
+                        {
+                            "title": 'CSS',
+                            "percent": 13
+                        },
+                        {
+                            "title": 'Javascript',
+                            "percent": 75
+                        },
+                        {
+                            "title": 'JQuery & Vue.js',
+                            "percent": 25
+                        }
+                    ]
+                },
+                {
+                    "category": "workflow",
+                    "tech": [
+                        {
+                            "title": 'GIT',
+                            "percent": 13
+                        },
+                        {
+                            "title": 'Terminal',
+                            "percent": 25
+                        },
+                        {
+                            "title": 'Gulp',
+                            "percent": 50
+                        },
+                        {
+                            "title": 'Webpack',
+                            "percent": 35
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+    components: {
+        'skillCircle': skillCircle,
+        'categoryRow': categoryRow
     }
 })
