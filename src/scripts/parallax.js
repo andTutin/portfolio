6049a4eeedc505bd7cc20f87e9357ff6732b5a13
Andrei <and.tutin@gmail.com>
2019-10-20 22:14:20 +0300

    parallax scroll

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,12 +1,12 @@
-const parallaxScroll = document.querySelector('.parallax-scroll'),
-      layers = parallaxScroll.children,
+const parallax = document.querySelector('.parallax'),
+      layers = parallax.children,
       windowWidth = document.body.clientWidth;
 if (windowWidth > 768) {
     window.addEventListener('scroll', () => {
         const scrollY = window.pageYOffset;
         Array.from(layers).forEach((layer, index) => {
-            if (index !=0 && index != 4) {
-                const transformY = `${scrollY / (-(Array.from(layers).length - index) * 7)}%`;
+            if (index != 4) {
+                const transformY = `${scrollY / (-(Array.from(layers).length - index) * 10)}%`;
                 layer.style.transform = `translateY(${transformY})`;
             }
         })
